467837aa6fe3d390bfb365a5696c4c8b02127587
Max Sorensen <medal4me2@gmail.com>
2017-03-07 17:55:49 +1100

    Redirect to login if not logged in.

--- src/components/nav/nav.component.ts
+++ src/components/nav/nav.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 import { Toon } from '../../models/toon.model';
 import { GuildRepository } from '../../repositories/guild.repository';
+import { UserRepository } from '../../repositories/user.repository';
 
 @Component({
   templateUrl: './nav.component.html',
 })
-export class NavComponent {
+export class NavComponent implements OnDestroy {
+  sub: Subscription;
   toons$: Observable<Toon[]>;
 
-  constructor(private guild: GuildRepository) {
+  constructor(private guild: GuildRepository, private users: UserRepository, private router: Router) {
     this.toons$ = this.guild.myToons$;
+
+    this.sub = this.users.me$.subscribe((user) => {
+      if (!user) {
+        this.router.navigate(['login']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
