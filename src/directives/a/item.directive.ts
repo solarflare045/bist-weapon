fc966e6c4c0023bcb7191d3ce3f14780793e4c13
Max Sorensen <medal4me2@gmail.com>
2017-03-09 15:09:30 +1100

    Items are now correctly coloured.

--- src/directives/a/item.directive.ts
+++ src/directives/a/item.directive.ts
@@ -5,6 +5,8 @@ import * as _ from 'lodash';
 import { CombineLatestAggregate } from '../../services/observable/observable';
 import { Gear } from '../../models/gear.model';
 
+const COLORS = ['#9d9d9d', '#9d9d9d', '#1eff00', '#0070dd', '#a335ee', '#ff8000', '#e6cc80', '#e6cc80'];
+
 @Directive({
   selector: 'a[appItemLink]',
 })
@@ -12,17 +14,25 @@ export class ItemLinkDirective implements OnDestroy {
   private gear$: Subject<Gear> = new Subject();
   private subscription: Subscription;
 
-  @HostBinding() rel;
+  @HostBinding('style.color') color: string;
+  @HostBinding() rel: string;
 
   @Input() set appItemLink(gear: Gear) { this.gear$.next(gear); }
+  @Input() set i(i: number) { this.color = COLORS[i]; }
 
   constructor() {
     this.subscription = this.gear$
-      .switchMap((gear) => Observable.combineLatest( gear.id$, gear.level$, gear.enchant$, gear.gems$ ))
-      .map(([ id, level, enchant, gems ]) => {
+      .switchMap((gear) =>
+        Observable.combineLatest( gear.id$, gear.level$, gear.enchant$, gear.gems$, gear.bonuses$, gear.upgrade$, gear.quality$ ),
+      )
+      .map(([ id, level, enchant, gems, bonuses, upgrade, quality ]) => {
+        this.color = COLORS[quality];
+
         let rel = `item=${ id }&ilvl=${ level }`;
-        if (enchant) { rel += `&ench=${ enchant }`; }
-        if (gems.length) { rel += `&gems=${ gems.join(':') }`; }
+        if (enchant)        { rel += `&ench=${ enchant }`           ; }
+        if (gems.length)    { rel += `&gems=${ gems.join(':') }`    ; }
+        if (bonuses.length) { rel += `&bonus=${ bonuses.join(':') }`; }
+        if (upgrade)        { rel += `&upgd=${ upgrade }`           ; }
         return rel;
       })
       .do((rel) => {
--- src/models/gear.model.ts
+++ src/models/gear.model.ts
@@ -7,7 +7,9 @@ export class Gear extends SharedNode {
   private _id: SharedValue<string> = this.child('id').asValue();
   private _level: SharedValue<number> = this.child('itemLevel').asValue();
   private _name: SharedValue<string> = this.child('name').asValue();
+  private _quality: SharedValue<string> = this.child('quality').asValue();
   private _slot: SharedValue<string> = this.child('slot').asValue();
+  private _upgrade: SharedValue<number> = this.child('tooltipParams').child('upgrade').child('current').asValue();
 
   private _gems: Observable<number[]> = Observable.combineLatest(
     this.child('tooltipParams').child('gem0').asValue<number>().value$,
@@ -19,7 +21,10 @@ export class Gear extends SharedNode {
   public readonly id$ = this._id.value$;
   public readonly level$ = this._level.value$;
   public readonly name$ = this._name.value$;
+  public readonly quality$ = this._quality.value$;
   public readonly slot$ = this._slot.value$;
+  public readonly upgrade$ = this._upgrade.value$;
 
   public readonly gems$ = this._gems.map((gems) => _.compact(gems));
+  public readonly bonuses$: Observable<number[]> = this.child('bonusLists').asValue().value$.map((bonuses) => _.values(bonuses));
 }
